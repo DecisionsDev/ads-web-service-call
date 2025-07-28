875bb657810ba455744c8d2ebe45fe5e7da19dbf
jean-louisardoint <ardoint@fr.ibm.com>
2025-07-28 17:57:19 +0200

    Real unit tests

--- IPGeoLoc/src/test/java/org/ipgeoloc/IPAPITest.java
+++ IPGeoLoc/src/test/java/org/ipgeoloc/IPAPITest.java
@@ -1,40 +1,40 @@
 package org.ipgeoloc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
+
 public class IPAPITest {
-    public static void main(String[] args) throws IOException {
+
+    @Test
+    public void testIP_API_dot_com() {
         Optional<IPAPIAnswer> oprequest = IPAPIRequest.remoteCall(
                 "http://ip-api.com/json/" +"24.48.0.1" + "?fields=country,countryCode");
         if (oprequest.isPresent()) {
             IPAPIAnswer answer = oprequest.get();
-            System.out.println("Your IP is: " + answer.getIp());
-            System.out.println("Your country is: " + answer.getCountry());
-            System.out.println("Your country code is: " + answer.getCountryCode());
-            System.out.println("Your region is: " + answer.getRegion());
-            System.out.println("Your region name is: " + answer.getRegionName());
-            System.out.println("Your city is: " + answer.getCity());
-            System.out.println("Your zip code is: " + answer.getZip());
-            System.out.println("Your latitude is: " + answer.getLat());
-            System.out.println("Your longitude is: " + answer.getLon());
-            System.out.println("Your timezone is: " + answer.getTimezone());
-            System.out.println("Your ISP is: " + answer.getIsp());
-            System.out.println("Your organization is: " + answer.getOrg());
-            System.out.println("Your autonomous system is: " + answer.getAs());
-            System.out.println("Your query is: " + answer.getQuery());
+            assertEquals("Canada", answer.getCountry());
         }
 
-        try {
-            IPAPIRequest.ip2Country("24.48.0.1");
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
+    }
 
+    @Test
+    public void testWhenURLNotSet() {
+        // fallback for specific case (see IPAPIRequest code)
+        assertEquals("Canada", IPAPIRequest.ip2Country("131.132.0.0"));
+        // other case should not work
+        assertThrows(RuntimeException.class, () -> IPAPIRequest.ip2Country("24.48.0.1"));
+    }
+    
+    @Test
+    public void testRegularUsage() {
         IPAPIRequest.setURL("http://ip-api.com/json/");
-        System.out.println(IPAPIRequest.ip2Country("24.48.0.1"));
-        System.out.println(IPAPIRequest.ip2LatLong("24.48.0.1"));
+        assertEquals("Canada", IPAPIRequest.ip2Country("24.48.0.1"));
+        assertEquals("United States", IPAPIRequest.ip2Country("100.0.0.0"));
     }
     
 }
